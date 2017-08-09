651dde82c55cdc1f9314fd4e65130187f8a976b7
Matthew Cole <mcole8@binghamton.edu>
2017-08-09 12:16:07 -0400

    Fix argument parsing in front-end

--- ripe_tester.py
+++ ripe_tester.py
@@ -4,7 +4,7 @@
 # Released under the MIT license (see file named LICENSE)
 #
 # This program is part the paper titled
-# RIPE: Runtime Intrusion Prevention Evaluator 
+# RIPE: Runtime Intrusion Prevention Evaluator
 # Authored by: John Wilander, Nick Nikiforakis, Yves Younan,
 #              Mariam Kamkar and Wouter Joosen
 # Published in the proceedings of ACSAC 2011, Orlando, Florida
@@ -13,36 +13,33 @@
 
 import os
 import sys
+import argparse
 
 code_ptr = ["ret", "baseptr",
- "funcptrstackvar", "funcptrstackparam",
- "funcptrheap", "funcptrbss", "funcptrdata",
-"longjmpstackvar", "longjmpstackparam",
-"longjmpheap", "longjmpbss", "longjmpdata",  "structfuncptrstack","structfuncptrheap",
-                         "structfuncptrdata","structfuncptrbss"] ;
+	"funcptrstackvar", "funcptrstackparam",
+	"funcptrheap", "funcptrbss", "funcptrdata",
+	"longjmpstackvar", "longjmpstackparam",
+	"longjmpheap", "longjmpbss", "longjmpdata",
+	"structfuncptrstack","structfuncptrheap",
+    "structfuncptrdata","structfuncptrbss"] ;
 
 funcs = ["memcpy", "strcpy", "strncpy", "sprintf", "snprintf",
-	 "strcat", "strncat", "sscanf", "fscanf", "homebrew"];
+	"strcat", "strncat", "sscanf", "fscanf", "homebrew"];
 
 
 locations = ["stack","heap","bss","data"];
 attacks = ["createfile", "returnintolibc", "rop"];
 
-techniques = []
-repeat_times = 0
-
-
-if len(sys.argv) < 2:
-	print "Usage: python "+sys.argv[0] + "[direct|indirect|both] <number of times to repeat each test>"
-	sys.exit(1)
-
+# Parse arguments from the command line
+parser = argparse.ArgumentParser(description='Automated testing using the RIPE evaluation tool.')
+parser.add_argument('techniques', default='both', choices=['direct', 'indirect', 'both'], help='Overflow technique to test.')
+parser.add_argument('repeat_times', type=int, help='Number of times to repeat each benchmark.')
+args = parser.parse_args()
+if args.techniques == 'both':
+	techniques = ['direct', 'indirect']
 else:
-	if sys.argv[1] == "both":
-		techniques = ["direct","indirect"];
-	else:
-		techniques = [sys.argv[1]];
-
-	repeat_times = int(sys.argv[2]);
+	techniques = [args.techniques]
+repeat_times = args.repeat_times
 
 
 i = 0
@@ -70,7 +67,7 @@ for attack in attacks:
 						cmdline = "./build/ripe_attack_generator -t "+tech+" -i "+attack+" -c " + ptr + "  -l " + loc +" -f " + func + " > /tmp/ripe_log 2>&1"
 						os.system(cmdline)
 						log = open("/tmp/ripe_log","r")
-		
+
 
 						if log.read().find("Impossible") != -1:
 							print cmdline,"\t\t","NOT POSSIBLE"
@@ -79,7 +76,7 @@ for attack in attacks:
 
 
 						if os.path.exists("/tmp/rip-eval/f_xxxx"):
-							s_attempts += 1		
+							s_attempts += 1
 							os.system("rm /tmp/rip-eval/f_xxxx")
 
 
@@ -96,13 +93,7 @@ for attack in attacks:
 					else:
 						print cmdline,"\t\tSOMETIMES\t", s_attempts,"/",repeat_times
 						total_some +=1;
-						
+
 
 total_attacks = total_ok + total_some + total_fail + total_np;
 print "\n||Summary|| OK: ",total_ok," ,SOME: ",total_some," ,FAIL: ",total_fail," ,NP: ",total_np," ,Total Attacks: ",total_attacks
-
-						
-					
-
-
-
